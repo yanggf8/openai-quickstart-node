2d865b9d5246acc76efa6af5f44a3cec1a28a896
yanggf8 <yanggf@yahoo.com>
2023-03-13 13:45:10 +0800

    pacMan loading

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
+import { PacmanLoader } from "react-spinners";
 import styles from "./index.module.css";
-
 
 // --- Set somewhere else but within scope
 const url = "some url";
@@ -11,8 +11,12 @@ let interval = null;
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
+  const [lastAnimalInput, setLastAnimalInput] = useState("");
   const [result, setResult] = useState();
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  let [loading, setLoading] = useState(true);
+  //let [color, setColor] = useState("#ffffff");
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -24,7 +28,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ animal: anim alInput }),
       });
 
       const data = await response.json();
@@ -32,6 +36,7 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
       setResult(data.result);
+      setLastAnimalInput(animalInput);
       setAnimalInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
@@ -62,9 +67,14 @@ export default function Home() {
             onChange={(e) => setAnimalInput(e.target.value)}
             onKeyDown={(e) => { if(e.key === 'Enter') {onSubmit(e)}}}
           />
-          <input type="submit" value="Generate names" disabled={isSubmitting}/>
+          {/*<input type="submit" value="Generate names" disabled={isSubmitting}/>*/}
+          {isSubmitting ? (
+            <PacmanLoader color="#36d7b7" />
+              ) : (
+              <button type="submit">Generate names</button>
+              )}
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result}>{lastAnimalInput}:{result}</div>
       </main>
     </div>
   );
